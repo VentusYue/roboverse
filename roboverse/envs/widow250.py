ea023351ec2cac82da8599566637ef5a07e7095b
ventusyue <ventus@umich.edu>
2021-01-14 00:09:28 -0500

    merge, update parameters

--- roboverse/envs/widow250.py
+++ roboverse/envs/widow250.py
@@ -341,33 +341,6 @@ class Widow250Env(gym.Env, Serializable):
             self.grasp_success_object_gripper_threshold)
         return info
 
-    def get_dataset(self, h5path=None):
-        # if h5path is None:
-        #     if self._dataset_url is None:
-        #         raise ValueError("Offline env not configured with a dataset URL.")
-        #     h5path = download_dataset_from_url(self.dataset_url)
-
-        dataset_file = h5py.File(h5path, 'r')
-        data_dict = {k: dataset_file[k][:] for k in get_keys(dataset_file)}
-        dataset_file.close()
-
-        # Run a few quick sanity checks
-        for key in ['observations', 'actions', 'rewards', 'terminals']:
-            assert key in data_dict, 'Dataset is missing key %s' % key
-        N_samples = data_dict['observations'].shape[0]
-        if self.observation_space.shape is not None:
-            assert data_dict['observations'].shape[1:] == self.observation_space.shape, \
-                    'Observation shape does not match env: %s vs %s' % (str(data_dict['observations'].shape[1:]), str(self.observation_space.shape))
-        assert data_dict['actions'].shape[1:] == self.action_space.shape, \
-                    'Action shape does not match env: %s vs %s' % (str(data_dict['actions'].shape[1:]), str(self.action_space.shape))
-        if data_dict['rewards'].shape == (N_samples, 1):
-            data_dict['rewards'] = data_dict['rewards'][:,0]
-        assert data_dict['rewards'].shape == (N_samples,), 'Reward has wrong shape: %s' % (str(data_dict['rewards'].shape))
-        if data_dict['terminals'].shape == (N_samples, 1):
-            data_dict['terminals'] = data_dict['terminals'][:,0]
-        assert data_dict['terminals'].shape == (N_samples,), 'Terminals has wrong shape: %s' % (str(data_dict['rewards'].shape))
-        return data_dict
-
     def render_obs(self):
         img, depth, segmentation = bullet.render(
             self.observation_img_dim, self.observation_img_dim,
